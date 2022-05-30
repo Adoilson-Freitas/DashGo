f65bb783c90bb249f1804306d94319b5490175d7
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-05-30 10:19:23 -0300

    Create Paging Logic

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -2,7 +2,6 @@ import { createServer, Factory, Model } from "miragejs";
 import faker from "faker";
 
 type User = {
-  users: string;
   name: string;
   email: string;
   created_at: string;
@@ -17,26 +16,44 @@ export function makeServer() {
     factories: {
       user: Factory.extend({
         name() {
-          return `${faker.name.firstName()} ${faker.name.lastName()}`
+          return `${faker.name.firstName()} ${faker.name.lastName()}`;
         },
         email() {
           return faker.internet.email().toLocaleLowerCase();
         },
         createdAt() {
-          return faker.date.recent(10)
-        }
-      })
+          return faker.date.recent(10);
+        },
+      }),
     },
 
     seeds(server) {
-      server.createList("user", 10);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, 
+          { "x-total-count": String(total) }, 
+          { users }
+        )
+      });
+
       this.post("/users");
 
       this.namespace = "";
