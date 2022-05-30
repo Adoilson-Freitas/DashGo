ac4ac88d3e58fb6a27b8f9e4d2551773144a0698
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-05-30 12:04:45 -0300

    Switch pages

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -22,7 +22,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 
   const totalCount = Number(headers['x-total-count'])
   
-  const users = data.users.map(user => {
+  const users = data.users.map((user) => {
     return {
       id: user.id,
       name: user.name,
@@ -43,6 +43,6 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 
 export function useUsers(page: number) {
   return useQuery(['users', page], () => getUsers(page), {
-    staleTime: 1000 * 60 * 10, // 10 minutos
+    staleTime: 1000 * 2, // 2 segundos
   })
 }
